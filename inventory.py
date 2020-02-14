12ef2b78b3eb5973e99b99c435cf4c0cdefb9d1e
AlexanderGonzalezMMG <alex@malonemediagroup.com>
2020-02-14 13:31:19 -0600

    Added Category changes, need to add class structure

--- inventory.py
+++ inventory.py
@@ -1,33 +1,24 @@
+# !/usr/local/bin/env python3.8
+import tkinter as tk
+from tkinter import *
 from tkinter.filedialog import asksaveasfilename
 from tkinter.filedialog import askopenfilenames
 from tkinter import ttk
-from tkinter import *
 import numpy as np
 from pathlib import Path
 import pandas as pd
 import os
 import sys
-# !/usr/local/bin/env python3.8
-
+import csv
 # TODO: Add options to remove products, move category, add products, and etc.
-# TODO: Create app window manager (main class), export into exe for mac/windows.
-# TODO: Start github and readme for project. Document every function
-
-root = Tk()
-initDir = "/Users/alexgonzalez/Desktop/"
-files = [('CSV Files', '*.csv'), ('Text Document', '*.txt')]
-
+# TODO: Create app window manager (main class).
+# TODO: Document every function
+# TODO: export into exe for mac/windows
 
 """
 save is a function that is used to grab the filepath from the user in order to save
 the modified data frames to csv
 """
-
-
-def save():
-    file_name = asksaveasfilename(filetypes=files, defaultextension=files)
-    return file_name
-
 
 """
 remove_inventory is a function that iterates over an inventory csv file, if a SKU # is in 
@@ -36,6 +27,15 @@ the Remove xlsx file, the function will remove the row correspoding to the SKU #
 :param dfr: Products to be removed xlsx file (Dataframe)
 """
 
+root = tk.Tk()
+initDir = "~/Desktop/Git/inventory-scripts/data"
+files = [('CSV Files', '*.csv'), ('Excel Files',
+                                  '*.xlsx'), ('Text Files', '*.txt')]
+
+
+def save():
+    file_name = asksaveasfilename(filetypes=files, defaultextension=files)
+    return file_name
 
 def remove_inventory(df, dfr):
     for row in df.itertuples():
@@ -45,27 +45,45 @@ def remove_inventory(df, dfr):
     file_name = save()
     df.to_csv(file_name, index=False)
 
+def change_to_clearance(df,dfc):
+    for row in df.itertuples():
+        for sku in dfc.sku:
+            if(row.sku == sku):
+                if row.categories != 'Default Category/Clearance,Landmark Categories/Clearance':
+                    df.loc['categories'] = 'Default Category/Clearance,Landmark Categories/Clearance'
+                else:
+                    print("Category: [{}] ".format(row.categories))
+                    
+    return df
 
-# TODO: clean this portion, add to main app class.
-# ! Code Reads in Inventory CSV.
+
 root.inv_filepath = askopenfilenames(
-    initialdir=initDir, title="choose your inventory file")
+    initialdir=initDir, title="choose your inventory file", filetypes=files)
 inv_filepath = ''.join(root.inv_filepath)
 root.withdraw()
 
-df_inventory = pd.read_csv(inv_filepath, engine="python", sep=',', quotechar='"', error_bad_lines=False)
+df_inv = pd.read_csv(inv_filepath, engine="python", sep=',', quotechar='"', error_bad_lines=False)
 
-
-
-
-# root.rem_filepath = askopenfilenames(
-#     initialdir=initDir, title="choose your product removal file")
-# rem_filepath = ''.join(root.rem_filepath)
-# root.withdraw()
-
-# print(inv_filepath, rem_filepath)
-
+root.rem_filepath = askopenfilenames(
+    initialdir=initDir, title="choose your product removal file")
+rem_filepath = ''.join(root.rem_filepath)
+root.withdraw()
 
 # df_remove = pd.read_excel(rem_filepath, sheetName=None)
+# remove_inventory(df_inv, df_remove)
 
-# remove_inventory(df_inventory, df_remove)
+
+# TODO: Turn this into a function that will be called by a button to open Clearance CSV file.
+df_change = pd.ExcelFile(rem_filepath)
+
+for name in df_change.sheet_names:
+    df = df_change.parse(name)
+    if name == 'REMOVED':
+        print('Done!\n')
+        break
+    elif df.empty == True:
+        print('Processing: [{}] ...'.format(name), 'Empty!')
+    else:
+        print('Processing: [{}] ...'.format(name))
+        df_inv = change_to_clearance(df_inv, df)
+
--- testGUI.py
+++ testGUI.py
@@ -8,11 +8,11 @@ from pathlib import Path
 import pandas as pd
 import os
 import sys
+import csv
 # !/usr/local/bin/env python3.8
 
-# TODO: Add options to remove products, move category, add products, and etc.
-# TODO: Create app window manager (main class), export into exe for mac/windows.
-# TODO: Start github and readme for project. Document every function
+initDir = "/Users/alexgonzalez/Desktop/Git/inventory-scripts/data"
+files = [('CSV Files', '*.csv'), ('Text Document', '*.txt')]
 
 
 class Application(tk.Frame):
@@ -22,84 +22,28 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
-    def clean_inv(self):
-        root.inv_filepath = askopenfilenames(
-        initialdir=initDir, title="choose your inventory file")
-        inv_filepath = ''.join(root.inv_filepath)
-        root.withdraw()
-
-    def change_cat(self):
-        print("Changing Category")
-
-    def add_products(self):
-        print("Adding Products to Inv")
-
-    def remove_inventory(df, dfr):
-        for row in df.itertuples():
-            for remove_value in dfr.sku:
-                if(row.sku == remove_value):
-                    df = df.drop([row.Index])
-        file_name = asksaveasfilename(filetypes=files, defaultextension=files)
-        df.to_csv(file_name, index=False)
-
     def create_widgets(self):
-        self.clean_inv = tk.Button(self)
-        self.clean_inv["text"] = "Clean Inventory"
-        self.clean_inv["command"] = self.clean_inv
-        self.clean_inv.pack(side="top")
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Open CSV File"
+        self.hi_there["command"] = self.open_csv
+        self.hi_there.pack(side="top")
 
-        self.change_cat = tk.Button(self)
-        self.change_cat["text"] = "Change Category"
-        self.change_cat["command"] = self.change_cat
-        self.change_cat.pack(side="top")
-
-        self.add_products = tk.Button(self)
-        self.add_products["text"] = "Change Category"
-        self.add_products["command"] = self.add_products
-        self.add_products.pack(side="top")
-
-        self.quit = tk.Button(self, text="Cancel", fg="red",
+        self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 
+    def open_csv(self):
+        self.filepath = askopenfilenames(
+            initialdir=initDir, title="choose your inventory file")
+        self.filepath=''.join(self.filepath)
+        fp = open(self.filepath)
+        read = csv.reader(fp,delimiter = ",")
+        columns = next(read)
+        print
+        for name in columns:
+            new_label = tk.Label(self, text=name)
+            new_label.pack()
 
 root = tk.Tk()
 app = Application(master=root)
-app.master.title("Inventory System")
-app.master.geometry("300x300")
 app.mainloop()
-initDir = "/Users/alexgonzalez/Desktop/Git/"
-files = [('CSV Files', '*.csv'), ('Text Document', '*.txt')]
-
-
-"""
-save is a function that is used to grab the filepath from the user in order to save
-the modified data frames to csv
-"""
-
-
-"""
-remove_inventory is a function that iterates over an inventory csv file, if a SKU # is in
-the Remove xlsx file, the function will remove the row correspoding to the SKU #.
-:param df: Inventory CSV file (Dataframe)
-:param dfr: Products to be removed xlsx file (Dataframe)
-"""
-
-
-# TODO: clean this portion, add to main app class.
-
-
-
-
-root.rem_filepath = askopenfilenames(
-    initialdir=initDir, title="choose your product removal file")
-rem_filepath = ''.join(root.rem_filepath)
-root.withdraw()
-
-print(inv_filepath, rem_filepath)
-
-df_inventory = pd.read_csv(
-    inv_filepath, engine="python", sep=',', quotechar='"', error_bad_lines=False)
-df_remove = pd.read_excel(rem_filepath, sheetName=None)
-
-remove_inventory(df_inventory, df_remove)
