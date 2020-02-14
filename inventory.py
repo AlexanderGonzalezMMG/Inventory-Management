31052cd9ad314da28a215145f047f09a3b59c799
AlexanderGonzalezMMG <alex@malonemediagroup.com>
2020-02-14 14:33:18 -0600

    branch init

--- inventory.py
+++ inventory.py
@@ -27,11 +27,36 @@ the Remove xlsx file, the function will remove the row correspoding to the SKU #
 :param dfr: Products to be removed xlsx file (Dataframe)
 """
 
-root = tk.Tk()
 initDir = "~/Desktop/Git/inventory-scripts/data"
 files = [('CSV Files', '*.csv'), ('Excel Files',
                                   '*.xlsx'), ('Text Files', '*.txt')]
 
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Malone Inventory Management")
+        self.geometry("500x500")
+
+        self.menubar = tk.Menu(self, bg="lightgrey", fg="white")
+        self.file_menu = tk.Menu(self.menubar, tearoff=0, bg="lightgrey", fg="white")
+        
+        self.file_menu.add_command(label="Open", command=self.file_open, accelerator="Ctrl+O")
+        self.file_menu.add_command(label="Save", command=self.file_open, accelerator="Ctrl+S")
+
+        self.menubar.add_cascade(label="File", menu=self.file_menu)
+        self.config(menu=self.menubar)
+        
+        
+    def file_open(self, event=None):
+        file = askopenfilenames()
+        
+        while file and not file.endswith(".csv"):
+            msg.showerror("Wrong File Type, please load an csv or xlsx file!")
+            file = askopenfilenames()
+        
 
 def save():
     file_name = asksaveasfilename(filetypes=files, defaultextension=files)
@@ -57,33 +82,38 @@ def change_to_clearance(df,dfc):
     return df
 
 
-root.inv_filepath = askopenfilenames(
-    initialdir=initDir, title="choose your inventory file", filetypes=files)
-inv_filepath = ''.join(root.inv_filepath)
-root.withdraw()
-
-df_inv = pd.read_csv(inv_filepath, engine="python", sep=',', quotechar='"', error_bad_lines=False)
-
-root.rem_filepath = askopenfilenames(
-    initialdir=initDir, title="choose your product removal file")
-rem_filepath = ''.join(root.rem_filepath)
-root.withdraw()
-
-# df_remove = pd.read_excel(rem_filepath, sheetName=None)
-# remove_inventory(df_inv, df_remove)
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
 
 
-# TODO: Turn this into a function that will be called by a button to open Clearance CSV file.
-df_change = pd.ExcelFile(rem_filepath)
+# root.inv_filepath = askopenfilenames(
+#     initialdir=initDir, title="choose your inventory file", filetypes=files)
+# inv_filepath = ''.join(root.inv_filepath)
+# root.withdraw()
 
-for name in df_change.sheet_names:
-    df = df_change.parse(name)
-    if name == 'REMOVED':
-        print('Done!\n')
-        break
-    elif df.empty == True:
-        print('Processing: [{}] ...'.format(name), 'Empty!')
-    else:
-        print('Processing: [{}] ...'.format(name))
-        df_inv = change_to_clearance(df_inv, df)
+# df_inv = pd.read_csv(inv_filepath, engine="python", sep=',', quotechar='"', error_bad_lines=False)
+
+# root.rem_filepath = askopenfilenames(
+#     initialdir=initDir, title="choose your product removal file")
+# rem_filepath = ''.join(root.rem_filepath)
+# root.withdraw()
+
+# # df_remove = pd.read_excel(rem_filepath, sheetName=None)
+# # remove_inventory(df_inv, df_remove)
+
+
+# # TODO: Turn this into a function that will be called by a button to open Clearance CSV file.
+# df_change = pd.ExcelFile(rem_filepath)
+
+# for name in df_change.sheet_names:
+#     df = df_change.parse(name)
+#     if name == 'REMOVED':
+#         print('Done!\n')
+#         break
+#     elif df.empty == True:
+#         print('Processing: [{}] ...'.format(name), 'Empty!')
+#     else:
+#         print('Processing: [{}] ...'.format(name))
+#         df_inv = change_to_clearance(df_inv, df)
 
